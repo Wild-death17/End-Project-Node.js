da0e22985966ec4f8099df7815a868d9f6a72c66
Moshe <@Wild_death17>
2023-09-15 15:10:09 +0300

    Initial commit & BackEnd Setup

---
+++ Data-Base-SQL/Data-Base.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+const genParam = require("./Gen-Params");
+//------------------------------------------------
+let HOST = genParam.HOST;
+let USER = genParam.USER;
+let PASSWORD = genParam.PASSWORD;
+let DATABASE = genParam.DATABASE;
+console.log("database.HOST	=", HOST);
+console.log("database.USER	=", USER);
+console.log("database.PASSWORD=", PASSWORD);
+console.log("database.DATABASE=", DATABASE);
+//------------------------------------------------
+const pool = mysql.createPool({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+});
+//------------------------------------------------
+module.exports = {
+    pool: pool
+};
+//------------------------------------------------
