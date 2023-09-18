d58db960799a8a207f7e242f047b837f99ccab2f
Moshe <@Wild_death17>
2023-09-18 17:44:50 +0300

    Pages Routing Setup

--- Routs/Pages.js
+++ Routs/Pages.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 module.exports = router;
 //------------------------------------------------
+router.get("/", (req, res) => {
+    res.render("ClockToWork", {pageTitle: "ClockToWork"});
+});
 //------------------------------------------------
+router.get("/Employees", (req, res) => {
+    res.render("Employees", {pageTitle: "Employees"});
+});
 //------------------------------------------------
+router.get("/WorkingHours", (req, res) => {
+    res.render("WorkingHours", {pageTitle: "WorkingHours"});
+});
 //------------------------------------------------
